c619c5263f3d23771946297901a7867da6232628
maxiaohao <maxiaohao@gmail.com>
2021-02-07 20:44:24 +1000

    Finish fn_link

--- refresh-symlinks.sh
+++ refresh-symlinks.sh
@@ -3,10 +3,14 @@
 fn_link() {
   raw_filename=$1
   full_raw_filename=$(readlink -f $raw_filename)
-  full_target_filename="$HOME/${raw_filename//__/\/}"
-  filename=$(basename $full_target_filename)
-  dir=$(dirname $full_target_filename)
-  echo "full_raw=$full_raw_filename, target=$full_target_filename, filename=$filename, dir=$dir"
+  full_symlink_filename="$HOME/${raw_filename//__/\/}"
+  filename=$(basename $full_symlink_filename)
+  dir=$(dirname $full_symlink_filename)
+  # echo "full_raw=$full_raw_filename, symlink=$full_symlink_filename, filename=$filename, dir=$dir"
+  rm -f $full_symlink_filename
+  mkdir -p $dir
+  echo "Creating symlink: $full_symlink_filename -> $full_raw_filename ..."
+  ln -s $full_raw_filename $full_symlink_filename
 }
 
 for dotfile in .*; do
