f47941bafd1604d6a31c51e768a23baa51a66eb2
Kevin Ma <maxiaohao@gmail.com>
2026-05-11 13:58:24 +1000

    Disable treesitter to avoid needing cl.exe in lazyvim

--- .config__nvim/lua/plugins/disabled.lua
+++ .config__nvim/lua/plugins/disabled.lua
@@ -1,5 +1,7 @@
 return {
   { "dashboard-nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
   {
     "folke/snacks.nvim",
     opts = { dashboard = { enabled = false } },
